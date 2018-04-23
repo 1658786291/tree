406d5fe0aa3ec0e6a2389c4440a06c79fbb9350f
liul <1658786291@qq.com>
2018-04-23 14:53:31 +0800

    提交

--- 第三方App检测/第三方App检测/ViewController.m
+++ 第三方App检测/第三方App检测/ViewController.m
@@ -15,7 +15,7 @@
 @implementation ViewController
 - (IBAction)btn:(UIButton *)sender {
     
-    NSLog(@"点击啦");
+    NSLog(@"点击啦123");
 //    NSURL *url = [NSURL URLWithString:@"ChengShangGou://test"];
 //    if ([url.host isEqualToString:@"test"])
 //    {
